32357b299ba12f33638e697658457e9e4a03ed5a
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-06 20:37:35 -0500

    add the latest configuration

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+
+ function HomePage() {
+    return (
+      <>
+        <h1>Dreamy Jotter</h1>
+        <img src="ruta_de_tu_imagen.jpg" alt="Imagen" />
+        <button>Login</button>
+        <button>Sign Up</button>
+      </>
+    )
+  }
+  
+export default HomePage;
