df0b8ef576ec275ef27a78b45368d50f6b160de7
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-07 11:56:26 -0500

    07

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,34 @@
 import style from "../css-modules/HomePage.module.css";
-import iconoDJ from "../assets/dreamyJ-icon-nav.png"
- function HomePage() {
-    return (
-      <div className={style["container-home"]}>
-        <h1 className={style.title} >Dreamy Jotter</h1>
-        <img className={style.logo} src={iconoDJ} alt="Dreamy Jotter Logo" />
-        <div className={style["container-buttons"]}>
-        <button className={style["btn-login"]}>Login</button>
-        <button className={style["btn-sign"]}>Sign Up</button>
-        </div>
-        
+import iconoDJ from "../assets/icon-dj.png";
+import { Link } from "react-router-dom";
+function HomePage() {
+  return (
+    <div className={style["container-home"]}>
+      <img className={style.logo} src={iconoDJ} alt="Dreamy Jotter Logo" />
+      <div className={style["container-buttons"]}>
+        <button className={style["btn-google"]}>
+          <div className={style["conteiner-btn-google"]}>
+            <i
+              className="bx bxl-google"
+              style={{
+                marginRight: "8px",
+                width: "20px",
+                height: "20px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            />
+            <span>Continue with Google</span>
+          </div>
+        </button>
+
+        <span>or</span>
+        <button className={style["btn-light"]}>Email</button>
+        <button className={style["btn-light"]}>Password</button>
+        <Link className={style["btn"]} >Login</Link>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
+
 export default HomePage;
