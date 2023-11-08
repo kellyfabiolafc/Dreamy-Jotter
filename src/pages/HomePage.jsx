7478c9a6d9b5207cd5bcb8375de5986c0e96159e
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-07 20:52:51 -0500

    update commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,31 +3,40 @@ import iconoDJ from "../assets/icon-dj.png";
 import { Link } from "react-router-dom";
 function HomePage() {
   return (
-    <div className={style["container-home"]}>
-      <img className={style.logo} src={iconoDJ} alt="Dreamy Jotter Logo" />
-      <div className={style["container-buttons"]}>
-        <button className={style["btn-google"]}>
-          <div className={style["conteiner-btn-google"]}>
-            <i
-              className="bx bxl-google"
-              style={{
-                marginRight: "8px",
-                width: "20px",
-                height: "20px",
-                display: "flex",
-                alignItems: "center",
-              }}
-            />
-            <span>Continue with Google</span>
-          </div>
-        </button>
-
-        <span>or</span>
-        <button className={style["btn-light"]}>Email</button>
-        <button className={style["btn-light"]}>Password</button>
-        <Link className={style["btn"]} >Login</Link>
-      </div>
+    <div className={style.homeContainer}>
+    <img className={style.logo} src={iconoDJ} alt="Dreamy Jotter Logo" />
+    <form className={style.buttonContainer}>
+      <button className={style.googleButton}>
+        <div className={style.googleButtonContent}>
+          <i
+            className="bx bxl-google"
+            style={{
+              marginRight: "8px",
+              width: "40px",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+              color:"#CF8193",
+            }}
+          />
+          <span>Continue with Google</span>
+        </div>
+      </button>
+  
+      <span>or</span>
+  
+      <input className={style.emailInput} placeholder="Email" />
+      <input className={style.passwordInput} placeholder="Password" />
+      <button className={style.loginButton}>Login</button>
+      </form>
+      <div className={style.additionalOptions}>
+      <span>Did you forget your password ? <span className={style.forgotPasswordLink}>Get it back</span></span>
+      <span>You do not have an account? <span className={style.registerLink}>Sign up</span></span>
     </div>
+    
+   
+  </div>
+  
   );
 }
 
