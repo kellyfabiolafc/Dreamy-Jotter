712f4828edf7270b9cd19b3b1cf64216c6744102
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-08 19:48:33 -0500

    add new file

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,27 @@
-
-
-export default function RegisterPage() {
+import style from "../css-modules/LoginPage.module.css";
+import iconoDJ from "../assets/icon-dj.png";
+ function RegisterPage() {
   return (
-    <div>RegisterPage</div>
-  )
+    <div className={style.homeContainer}>
+      <img className={style.logo} src={iconoDJ} alt="Dreamy Jotter Logo" />
+      <p>Unlock your inner world on Dreamy Jotter. Capture your thoughts, memories, and reflections in a realm crafted just for you. Pen down your essence!</p>
+      <form className={style.buttonContainer}>
+        <input className={style.emailInput} placeholder="Username" />
+        <input className={style.emailInput} placeholder="Email" />
+        <input className={style.passwordInput} placeholder="Password" />
+        <button className={style.loginButton}>Create</button>
+      </form>
+      <div className={style.additionalOptions}>
+        <span>
+          Did you forget your password ?{" "}
+          <span className={style.forgotPasswordLink}>Get it back</span>
+        </span>
+        <span>
+          You do not have an account?{" "}
+          <span className={style.registerLink}>Sign up</span>
+        </span>
+      </div>
+    </div>
+  );
 }
+export default RegisterPage
