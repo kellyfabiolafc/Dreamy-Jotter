c6e627eebbe1342d6da17e60ae17258c24a0d4e3
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-17 13:05:44 -0500

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import MyRoutes from "./routers/routes";
 import { AuthContextProvider } from "/src/context/AuthContext.jsx";
-import Loader from "./components/Loader";
-import { useState,useEffect } from "react";
+
 function App() {
   // const {user,logOut}= UserAuth
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simula una breve demora para propósitos de demostración
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000); // Puedes ajustar la duración de la simulación según tus necesidades
-  }, []);
-
-  if (loading) {
-    // Muestra el Loader mientras se carga la página
-    return <Loader />;
-  }
+  
   return (
     <>
       {" "}
