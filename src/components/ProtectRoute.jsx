5a127d579b40076feca83846b74b8c956dcd8dfb
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-17 14:06:47 -0500

    update commit

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { UserAuth } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
-function ProtectRoute({children}) {
-    const {user }= UserAuth();
-    if (!user){
-        return <Navigate to={"/"}/>
-    }
-    return children;
+
+// Define un componente llamado ProtectRoute que verifica si el usuario está autenticado
+function ProtectRoute({ children }) {
+  // Obtiene la información de autenticación del contexto
+  const { user } = UserAuth();
+
+  // Verifica si el usuario está autenticado
+  if (!user) {
+    // Si el usuario no está autenticado, redirige a la página de inicio ("/")
+    return <Navigate to={"/"} />;
+  }
+
+  // Si el usuario está autenticado, permite que los componentes hijos se rendericen
+  return children;
 }
 
 export default ProtectRoute;
