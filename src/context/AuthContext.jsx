5e8ca7d4c396d9b4dc07ffca7b5f77c70cd8ffe7
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-17 14:06:02 -0500

    add comments

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,32 +7,48 @@ import {
 } from "firebase/auth";
 
 import { auth } from "../services/fireBaseConfig";
+
+// Crea un contexto de autenticación
 const AuthContext = createContext();
+
+// Define un componente llamado AuthContextProvider que proporcionará el contexto de autenticación a sus hijos
 const AuthContextProvider = ({ children }) => {
+  // Estado local para almacenar la información del usuario
   const [user, setUser] = useState({});
+
+  // Función para iniciar sesión con Google
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
   };
+
+  // Función para cerrar sesión
   const logOut = () => {
     return signOut(auth);
   };
 
+  // Efecto de efecto secundario que escucha cambios en la autenticación del usuario
   useEffect(() => {
-    const unsubcribe = onAuthStateChanged(auth, (currenUser) => {
-      setUser(currenUser);
+    // Registra un observador en la autenticación para rastrear cambios en el usuario actual
+    const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
     });
+
+    // Limpia el observador cuando el componente se desmonta o cuando cambia la dependencia
     return () => {
       unsubcribe();
     };
-  }, []);
+  }, []); // El efecto se ejecuta solo una vez al montar el componente
 
+  // Renderiza el contexto de autenticación y pasa sus valores y funciones como propiedades a los componentes hijos
   return (
     <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+// Función que permite a los componentes hijos acceder al contexto de autenticación
 const UserAuth = () => {
   return useContext(AuthContext);
 };
