4a3f0ee06f183356b2097e260348578501f406a6
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-17 09:37:40 -0500

    update context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,38 +1,40 @@
-import { useContext,createContext,useEffect,useState } from "react";
-import { GoogleAuthProvider, signOut ,onAuthStateChanged, signInWithPopup} from "firebase/auth";
+import { useContext, createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
 
-import {auth} from "../services/fireBaseConfig";
-const AuthContext=createContext();
- const AuthContextProvider= ({children})=>{
-    const [user, setUser] = useState({});
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth,provider);
-    } 
-    const logOut = ()=>{
-        signOut(auth);
-    }
+import { auth } from "../services/fireBaseConfig";
+const AuthContext = createContext();
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
 
-    useEffect(()=>{
-        const unsubcribe= onAuthStateChanged(auth,(currenUser)=> {
-            setUser(currenUser);
-        })
-        return ()=> {
-            unsubcribe();
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currenUser) => {
+      setUser(currenUser);
+    });
+    return () => {
+      unsubcribe();
+    };
+  }, []);
 
-        }
-    },[]);
+  return (
+    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+const UserAuth = () => {
+  return useContext(AuthContext);
+};
 
-    return (
-      
-        <AuthContext.Provider value = {{googleSignIn,logOut,user}}>
-        {children}
-        </AuthContext.Provider>
-     
-    )
-}
-const UserAuth = ()=>{
-    return useContext(AuthContext)
-}
-
-export  {UserAuth , AuthContextProvider }
+export { UserAuth, AuthContextProvider };
