9a40ad87168f1e251483df9532f958a9354d1792
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-11-29 11:36:20 -0500

    up commit

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -23,7 +23,7 @@ catch ( error){
         <img src={logo} alt="Dreamy Jotter Logo" className={styles.logo} />
         <div className={styles.title}>Dreamy Jotter</div>
       </div>
-   <button onClick={logOutt} className={styles.iconlogOut}><box-icon  name='log-out'></box-icon></button> 
+   <div onClick={logOutt} className={styles.iconlogOut}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAtklEQVR4nO2UMQ7CMAxFcxwG2JjgAlQcKjYnAJszAQO14QoFFcFUTlAUBFuLmpQMkfolb/F/sh19Ywb5SJGWAnRV5DqkBPkiSFkrwD0INdcvBKj4MUE/c/1UUoC9AlVxAEA713Oy67Eg3WMAKmfeBPnfioAf5xVPXO/R0kiBS2+A2M3MdFRueRowAc0jA7j7ipBuCR0ZYn9TfOfOQZGfSUdF7QWIHteClPWBCFCR2+2iFTDINOgFKO1J+kBxedIAAAAASUVORK5CYII="/></div> 
   </header>
   )
 }
