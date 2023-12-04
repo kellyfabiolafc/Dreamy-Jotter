c3a51b2c2f8fb48efd433846190094e79f8d98ff
Kelly fabiola <floresdelacruzkellyfabiola@gmail.com>
2023-12-03 21:28:35 -0500

    update commit

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -6,53 +6,70 @@ import { UserAuth } from '../context/AuthContext';
 import AddButton from './formElement/AddButton';
 
 function NoteList({ notes, showAddNoteFormHandler }) {
-  const { user } = UserAuth();
-  const [userNotes, setUserNotes] = useState([]);
-  const [editNoteId, setEditNoteId] = useState(null);
+  // Aquí estamos utilizando la función `UserAuth` para obtener la información del usuario.
+// Destructuramos el objeto resultante para obtener la propiedad `user`.
+const { user } = UserAuth();
 
-  useEffect(() => {
-    const filteredNotes = notes.filter((note) => note.user === user?.uid);
-    setUserNotes(filteredNotes);
-  }, [notes, user]);
+// Utilizamos el estado para almacenar las notas del usuario y el ID de la nota que se está editando.
+const [userNotes, setUserNotes] = useState([]);
+// Inicialmente, se establece en `null` ya que no hay ninguna nota en modo de edición.
+const [editNoteId, setEditNoteId] = useState(null);
 
-  const handleEditNote = (id) => {
-    setEditNoteId(id);
-  };
+// Este efecto se ejecuta cuando cambian las dependencias `notes` o `user`.
+// Filtramos las notas para obtener solo aquellas que pertenecen al usuario actual y las almacenamos en el estado.
+useEffect(() => {
+  const filteredNotes = notes.filter((note) => note.user === user?.uid);
+  setUserNotes(filteredNotes);
+}, [notes, user]);
 
-  const handleCancelEdit = () => {
+// Manejador para establecer el ID de la nota que se va a editar.
+const handleEditNote = (id) => {
+  setEditNoteId(id);
+};
+
+// Manejador para cancelar la edición, estableciendo el ID de edición de nota de nuevo a `null`.
+const handleCancelEdit = () => {
+  setEditNoteId(null);
+};
+
+// Función para manejar la presentación del formulario de edición
+const handleEditFormSubmit = async (editedContent) => {
+  try {
+    // Registra en la consola el intento de edición con el ID de la nota
+    console.log('Editar nota con ID:', editNoteId);
+
+    // Llama a la función para editar la nota utilizando el servicio `updateNote`
+    await updateNote(editNoteId, { data: editedContent });
+
+    // Se actualiza el estado de las notas después de la edición
+    setUserNotes((prevNotes) =>
+      prevNotes.map((note) => (note.id === editNoteId ? { ...note, data: editedContent } : note))
+    );
+
+    // Sale del modo de edición estableciendo el ID de edición de nota a `null`
     setEditNoteId(null);
-  };
+  } catch (error) {
+    // Manejamos cualquier error que pueda ocurrir durante la edición
+    console.error('Error al editar la nota:', error);
+  }
+};
 
-  const handleEditFormSubmit = async (editedContent) => {
-    try {
-      console.log('Editar nota con ID:', editNoteId);
+// Función para manejar la eliminación de una nota :P
+const handleDeleteNote = async (id) => {
+  try {
+    // Registra en la consola el intento de eliminación con el ID de la nota
+    console.log('Eliminar nota con ID:', id);
 
-      // Llama a la función para editar la nota
-      await updateNote(editNoteId, { data: editedContent });
+    // Llama a la función para eliminar la nota utilizando el servicio `deleteNote`
+    await deleteNote(id);
 
-      // Actualiza el estado después de la edición
-      setUserNotes((prevNotes) =>
-        prevNotes.map((note) => (note.id === editNoteId ? { ...note, data: editedContent } : note))
-      );
-
-      // Sale del modo de edición
-      setEditNoteId(null);
-    } catch (error) {
-      console.error('Error al editar la nota:', error);
-    }
-  };
-
-  const handleDeleteNote = async (id) => {
-    try {
-      console.log('Eliminar nota con ID:', id);
-      await deleteNote(id);
-
-      // Actualiza el estado después de la eliminación
-      setUserNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
-    } catch (error) {
-      console.error('Error al eliminar la nota:', error);
-    }
-  };
+    // Actualiza el estado de las notas después de la eliminación
+    setUserNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+  } catch (error) {
+    // Maneja cualquier error que pueda ocurrir durante la eliminación
+    console.error('Error al eliminar la nota:', error);
+  }
+};
 
   return (
     <>
